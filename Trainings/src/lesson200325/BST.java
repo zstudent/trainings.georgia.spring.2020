b0f01ef90a6f9bac38847c61fb47ef3e322ddba4
Zaal <zaal@quantori.com>
2020-03-25 15:48:07 +0400

    lesson 2020-03-25 part 2

---
+++ Trainings/src/lesson200325/BST.java
@@ -0,0 +1,53 @@
+package lesson200325;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BST implements Iterable<String>{
+	
+	private static class Node {
+		private String key;
+		private String value;
+		private Node left, right;
+		public Node(String key, String value) {
+			super();
+			this.key = key;
+			this.value = value;
+		}
+	}
+	
+	private Node root;
+	
+	public void put(String key, String value) {
+		root = put(root, key, value);
+	}
+
+	private Node put(Node x, String key, String value) {
+		if (x == null) return new Node(key, value);
+		
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0) 
+			x.left = put(x.left, key, value);
+		else if (cmp > 0) 
+			x.right = put(x.right, key, value);
+		else x.value = value;
+		
+		return x;
+	}
+
+	@Override
+	public Iterator<String> iterator() {
+		Queue<String> q = new LinkedList<String>();
+		inOrder(root, q);
+		return q.iterator();
+	}
+
+	private void inOrder(Node x, Queue<String> q) {
+		if (x == null) return;
+		inOrder(x.left, q);
+		q.add(x.key);
+		inOrder(x.right, q);
+	}
+
+}
---
+++ Trainings/src/lesson200325/UseBST.java
@@ -0,0 +1,19 @@
+package lesson200325;
+
+public class UseBST {
+	
+	public static void main(String[] args) {
+		BST bst = new BST();
+		
+		bst.put("Bob", "Bob");
+		bst.put("Sue", "Sue");
+		bst.put("Mike", "Mike");
+		bst.put("Alice", "Alice");
+		bst.put("Ann", "Ann");
+		
+		for (String key : bst) {
+			System.out.println(key);
+		}
+	}
+
+}
