16c1c0f33ede0dbc5f0a521a945df70434f918ff
Zaal <zaal@quantori.com>
2020-03-25 12:55:42 +0400

    UIExample

---
+++ Trainings/src/lesson200325/UIExample.java
@@ -0,0 +1,20 @@
+package lesson200325;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class UIExample {
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("My Program");
+		
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+		
+		
+	}
+
+}
