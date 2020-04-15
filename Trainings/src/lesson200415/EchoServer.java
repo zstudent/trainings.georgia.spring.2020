e2bf4d3ee42fac0d1b66e913b98aae84e45b7548
Zaal <zaal@quantori.com>
2020-04-15 15:40:50 +0400

    lesson 2020-04-15.  Trainings outline and Networking programming intro

---
+++ Trainings/src/lesson200415/EchoServer.java
@@ -0,0 +1,43 @@
+package lesson200415;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+
+	public static void main(String[] args) {
+		System.out.println("Start echo server");
+		try {
+			ServerSocket ss = new ServerSocket(10000);
+			while (true) {
+				Socket socket = ss.accept();
+				System.out.println(socket);
+				new Thread(() -> {
+					try {
+						communicate(socket);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}).start();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void communicate(Socket socket) throws IOException {
+		PrintWriter pw = new PrintWriter(socket.getOutputStream());
+		Scanner scanner = new Scanner(socket.getInputStream());
+
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			System.out.println(" > " + line);
+			pw.write(line + "\n");
+			pw.flush();
+		}
+	}
+
+}
