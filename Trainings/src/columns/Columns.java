afe0e26d163ce9b6dd6cba313dd0abc8a7e555f1
gugamaxara <gugamaxara@github.com>
2020-05-10 19:29:44 +0400

    graphic parts moved to View java file, fixed capital letter naming to java's standard naming, inlined speed logic

--- Trainings/src/columns/Columns.java
+++ Trainings/src/columns/Columns.java
@@ -11,8 +11,8 @@ import java.util.*;
 public class Columns extends Applet{
     static final int
     SL=25,
-    Depth=15,
-    Width=7,
+    HEIGHT=15,
+    WIDTH=7,
     MaxLevel=7,
     TimeShift=250,
     FigToDrop=33,
@@ -29,18 +29,19 @@ public class Columns extends Applet{
     Font fCourier;
     Figure Fig;
     int newField[][],oldField[][];
-    boolean NoChanges = true, KeyPressed = false;
-    Graphics _gr;
+    boolean NoChanges = true, paused = false;
+    Graphics graphics;
+    View view;
     
     
-    void CheckNeighbours(int a, int b, int c, int d, int i, int j) {
+    void checkNeighbours(int a, int b, int c, int d, int i, int j) {
         if ((newField[j][i]==newField[a][b]) && (newField[j][i]==newField[c][d])) {
             oldField[a][b] = 0;
-            DrawBox(a,b,8);
+            view.DrawBox(a,b,8);
             oldField[j][i] = 0;
-            DrawBox(j,i,8);
+            view.DrawBox(j,i,8);
             oldField[c][d] = 0;
-            DrawBox(c,d,8);
+            view.DrawBox(c,d,8);
             NoChanges = false;
             Score += (Level+1)*10;
             k++;
@@ -52,69 +53,31 @@ public class Columns extends Applet{
         }
         catch (InterruptedException e) {};
     }
-    void DrawBox(int x, int y, int c) {
-        if (c==0) {
-            _gr.setColor(Color.black);
-            _gr.fillRect(LeftBorder+x*SL-SL, TopBorder+y*SL-SL, SL, SL);
-            _gr.drawRect(LeftBorder+x*SL-SL, TopBorder+y*SL-SL, SL, SL);
-        }
-        else if (c==8) {
-            _gr.setColor(Color.white);
-            _gr.drawRect(LeftBorder+x*SL-SL+1, TopBorder+y*SL-SL+1, SL-2, SL-2);
-            _gr.drawRect(LeftBorder+x*SL-SL+2, TopBorder+y*SL-SL+2, SL-4, SL-4);
-            _gr.setColor(Color.black);
-            _gr.fillRect(LeftBorder+x*SL-SL+3, TopBorder+y*SL-SL+3, SL-6, SL-6);
-        }
-        else {
-            _gr.setColor(colorsList[c]);
-            _gr.fillRect(LeftBorder+x*SL-SL, TopBorder+y*SL-SL, SL, SL);
-            _gr.setColor(Color.black);
-            _gr.drawRect(LeftBorder+x*SL-SL, TopBorder+y*SL-SL, SL, SL);
-        }
-        //		g.setColor (Color.black);
-    }
-    void DrawField(Graphics g) {
-    	int i,j;
-        for (i=1; i<=Depth; i++) {
-            for (j=1; j<=Width; j++) {
-                DrawBox(j,i,newField[j][i]);
-            }
-        }
-    }
-    void DrawFigure(Figure f) {
-        DrawBox(f.x,f.y,f.c[1]);
-        DrawBox(f.x,f.y+1,f.c[2]);
-        DrawBox(f.x,f.y+2,f.c[3]);
-    }
-    void DropFigure(Figure f) {
+    void dropFigure(Figure f) {
     	int zz;
-        if (f.y < Depth-2) {
-            zz = Depth;
+        if (f.y < HEIGHT-2) {
+            zz = HEIGHT;
             while (newField[f.x][zz]>0) zz--;
-            DScore = (((Level+1)*(Depth*2-f.y-zz) * 2) % 5) * 5;
+            DScore = (((Level+1)*(HEIGHT*2-f.y-zz) * 2) % 5) * 5;
             f.y = zz-2;
         }
     }
-    boolean FullField() {
+    boolean isFieldFull() {
     	int i;
-        for (i=1; i<=Width; i++) {
+        for (i=1; i<=WIDTH; i++) {
             if (newField[i][3]>0)
                 return true;
         }
         return false;
     }
-    void HideFigure(Figure f) {
-        DrawBox(f.x,f.y,0);
-        DrawBox(f.x,f.y+1,0);
-        DrawBox(f.x,f.y+2,0);
-    }
+
     public void init() {
-        newField = new int[Width+2][Depth+2];
-        oldField = new int[Width+2][Depth+2];
-//        this.Fig = new Figure();
+        newField = new int[WIDTH+2][HEIGHT+2];
+        oldField = new int[WIDTH+2][HEIGHT+2];
         Level = 0;
         Score = 0;
-        _gr = getGraphics();
+        graphics = getGraphics();
+        this.view = new View(graphics);
         addKeyListener(new KeyAdapter() {
         	@Override
 			public void keyPressed(KeyEvent e) {
@@ -126,53 +89,56 @@ public class Columns extends Applet{
 					moveColumnRight();
 					break;
 				case KeyEvent.VK_DOWN:
-					moveCubeDown();
+					switchCubeDown();
 					break;
 				case KeyEvent.VK_UP:
-					moveCubeDown();
+					switchCubeDown();
 					break;
-				
 				case KeyEvent.VK_P:
-					KeyPressed = true;
-					ch = e.getKeyCode();
+					pauseGame();
 					break;
-				
 				case KeyEvent.VK_SPACE:
 					dropColumn();
 					break;
-					
+				case KeyEvent.VK_MINUS:
+					decreaseLevel();
+					break;
+				case KeyEvent.VK_EQUALS:
+					increaseLevel();
+					break;
 				default:
 					break;
 				}
 			}
+
         });
-        //TODO - MOVE THREAD HERE
+
         Thread thread = new Thread(() ->  {
-        	while(!FullField()) {
+        	while(!isFieldFull()) {
         		new Figure();
-        		DrawFigure(Fig);
-        		while((Fig.y<Depth-2) && (newField[Fig.x][Fig.y+3]==0)) {
+        		view.DrawFigure(Fig);
+        		while((Fig.y<HEIGHT-2) && (newField[Fig.x][Fig.y+3]==0)) {
         			try {
-						Thread.sleep(500);
+						Thread.sleep(MaxLevel - Level == 0 ? 300 : 300 * (MaxLevel - Level));
 					} catch (Exception e) {
 						// TODO: handle exception
 					}
-        			moveColumn();
+        			if(!paused) moveColumn();
         		};
-        		PasteFigure(Fig);
+        		pasteFigure(Fig);
                 do {
                     NoChanges = true;
-                    TestField();
+                    testField();
                     if (!NoChanges) {
                         Delay(500);
-                        PackField();
-                        DrawField(_gr);
+                        packField();
+                        view.DrawField(newField);
                         Score += DScore;
-                        ShowScore(_gr);
+                        view.ShowScore(Score);
                         if (k>=FigToDrop) {
                             k = 0;
                             if (Level<MaxLevel) Level++;
-                            ShowLevel(_gr);
+                            view.ShowLevel(Level);
                         }
                     }
                 } while (!NoChanges);
@@ -183,17 +149,17 @@ public class Columns extends Applet{
         
     }
 	public void moveColumn() {
-		if((Fig.y<Depth-2) && (newField[Fig.x][Fig.y+3]==0)) {
-			HideFigure(Fig);
+		if((Fig.y<HEIGHT-2) && (newField[Fig.x][Fig.y+3]==0)) {
+			view.HideFigure(Fig);
 			Fig.y++;
-			DrawFigure(Fig);        				
+			view.DrawFigure(Fig);			
 		}
 	}
-    void PackField() {
+    void packField() {
     	int i,j,n;
-        for (i=1; i<=Width; i++) {
-            n = Depth;
-            for (j=Depth; j>0; j--) {
+        for (i=1; i<=WIDTH; i++) {
+            n = HEIGHT;
+            for (j=HEIGHT; j>0; j--) {
                 if (oldField[i][j]>0) {
                     newField[i][n] = oldField[i][j];
                     n--;
@@ -205,52 +171,68 @@ public class Columns extends Applet{
     public void paint(Graphics g) {
         //		ShowHelp(g);
         
-        g.setColor(Color.black);
+//        g.setColor(Color.black);
         
-        ShowLevel(g);
-        ShowScore(g);
-        DrawField(g);
-        DrawFigure(Fig);
+        view.ShowLevel(Level);
+        view.ShowScore(Score);
+        view.DrawField(newField);
+        view.DrawFigure(Fig);
         requestFocus();
     }
-    void PasteFigure(Figure f) {
+    void pasteFigure(Figure f) {
         newField[f.x][f.y] = f.c[1];
         newField[f.x][f.y+1] = f.c[2];
         newField[f.x][f.y+2] = f.c[3];
     }
 
+	public void increaseLevel() {
+		if(Level < MaxLevel) Level++;
+		k = 0;
+		view.ShowLevel(Level);
+	}
+
+	public void decreaseLevel() {
+		if(Level > 0) Level--;
+		k = 0;
+		view.ShowLevel(Level);
+	}
+	public void pauseGame() {
+		if(paused) paused = false;
+		else paused = true;
+	}
+
 	public void dropColumn() {
-		HideFigure(Fig);
-		DropFigure(Fig);
-		DrawFigure(Fig);
+		view.HideFigure(Fig);
+		dropFigure(Fig);
+		view.DrawFigure(Fig);
 	}
 	public void moveColumnRight() {
-		if ((Fig.x<Width) && (newField[Fig.x+1][Fig.y+2]==0)) {
-		    HideFigure(Fig);
+		if ((Fig.x<WIDTH) && (newField[Fig.x+1][Fig.y+2]==0)) {
+			view.HideFigure(Fig);
 		    Fig.x++;
-		    DrawFigure(Fig);
+		    view.DrawFigure(Fig);
 		}
 	}
 	public void moveColumnLeft() {
 		if ((Fig.x>1) && (newField[Fig.x-1][Fig.y+2]==0)) {
-		    HideFigure(Fig);
+		    view.HideFigure(Fig);
 		    Fig.x--;
-		    DrawFigure(Fig);
+		    view.DrawFigure(Fig);
 		}
 	}
-	public void moveCubeDown() {
+	public void switchCubeDown() {
 		i = Fig.c[1];
 		Fig.c[1] = Fig.c[3];
 		Fig.c[3] = Fig.c[2];
 		Fig.c[2] = i;
-		DrawFigure(Fig);
+		view.DrawFigure(Fig);
 	}
 	public void switchCubeUp() {
 		i = Fig.c[1];
 		Fig.c[1] = Fig.c[2];
 		Fig.c[2] = Fig.c[3];
 		Fig.c[3] = i;
-		DrawFigure(Fig);
+		view.DrawFigure(Fig);
 	}
     void ShowHelp(Graphics g) {
         g.setColor(Color.black);
@@ -265,31 +247,20 @@ public class Columns extends Applet{
         g.drawString("Pause:           P",200-LeftBorder,180);
         g.drawString("Quit:     Esc or Q",200-LeftBorder,190);
     }
-    void ShowLevel(Graphics g) {
-        g.setColor(Color.black);
-        g.clearRect(LeftBorder+100,390,100,20);
-        g.drawString("Level: "+Level,LeftBorder+100,400);
-    }
-    void ShowScore(Graphics g) {
-        g.setColor(Color.black);
-        g.clearRect(LeftBorder,390,100,20);
-        g.drawString("Score: "+Score,LeftBorder,400);
-    }
-
-    void TestField() {
+    void testField() {
         int i,j;
-        for (i=1; i<=Depth; i++) {
-            for (j=1; j<=Width; j++) {
+        for (i=1; i<=HEIGHT; i++) {
+            for (j=1; j<=WIDTH; j++) {
                 oldField[j][i] = newField [j][i];
             }
         }
-        for (i=1; i<=Depth; i++) {
-            for (j=1; j<=Width; j++) {
+        for (i=1; i<=HEIGHT; i++) {
+            for (j=1; j<=WIDTH; j++) {
                 if (newField[j][i]>0) {
-                    CheckNeighbours(j,i-1,j,i+1,i,j);
-                    CheckNeighbours(j-1,i,j+1,i,i,j);
-                    CheckNeighbours(j-1,i-1,j+1,i+1,i,j);
-                    CheckNeighbours(j+1,i-1,j-1,i+1,i,j);
+                    checkNeighbours(j,i-1,j,i+1,i,j);
+                    checkNeighbours(j-1,i,j+1,i,i,j);
+                    checkNeighbours(j-1,i-1,j+1,i+1,i,j);
+                    checkNeighbours(j+1,i-1,j-1,i+1,i,j);
                 }
             }
         }
