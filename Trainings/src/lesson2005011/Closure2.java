d53014bc2bac0a60847185507ec9b50d5c07f244
Zaal <zaal@quantori.com>
2020-05-11 13:50:55 +0400

    lesson 2020-05-11 outline update; Java 8 part 1: closures

---
+++ Trainings/src/lesson2005011/Closure2.java
@@ -0,0 +1,30 @@
+package lesson2005011;
+
+import utils.Utils;
+
+public class Closure2 {
+	
+	static int y = 10;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("started");
+		
+		int x = 0;
+		
+		Runnable r = () -> { 
+			System.out.println("waiting..");
+			Utils.pause(5000);
+			System.out.println(x);
+			System.out.println(Closure2.y);
+		};
+		
+		y = 20;
+		
+		new Thread(r).start();
+		
+		System.out.println("finished");
+		
+	}
+
+}
