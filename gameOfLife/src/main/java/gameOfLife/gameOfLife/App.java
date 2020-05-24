d88e5e1ded2579a5c1fdf54244ef600e374b1e4f
Iraki Khaladze <i.khaladze1@gmail.com>
2020-05-24 19:57:30 +0400

    gitignore

---
+++ gameOfLife/src/main/java/gameOfLife/gameOfLife/App.java
@@ -0,0 +1,13 @@
+package gameOfLife.gameOfLife;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
